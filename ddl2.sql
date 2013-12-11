eea73ee09131435235dc022e20eabe962ea75e74
Kyle McGill <kylemcgill18@gmail.com>
2013-12-11 12:30:45 -0500

    finished, not tested

--- ddl2.sql
+++ ddl2.sql
@@ -1,12 +1,42 @@
-#DDL specs, second half
+# DDL specs, second half
 
-#Has_Auth
+# has struct info
+create table Has_Struct_Info
+  (MaterialID char(9) not null,
+   StructID   char(9) not null);
+
+# phase diagram
+create table Phase_Diagram
+  (MaterialID            char(9) not null,
+   PhaseDiagram          blob not null,
+   PublicationYear       integer,
+   DiagramType           varchar(30),
+   ConcentrationRange    varchar(50),
+   Temperature           varchar(20),
+   NatureOfInvestigation varchar(20),
+   DetailsID             char(9),
+   primary key(MaterialID));
+
+# diagram details
+create table Diagram_Details
+  (MaterialID      char(9) not null,
+   APDICDiagram    tinyint(1),
+   UniqueID        char(9) not null,
+   Title           varchar(50),
+   Publication     varchar(50),
+   Language        varchar(20),
+   OriginalDiagram varchar(50),
+   OriginalScope   varchar(50),
+   OriginalSize    varchar(10),
+   Remarks         varchar(50),
+   primary key(MaterialID));
+
+# Has_Auth
 create table Has_Auth
-  (Diag_ID     char(9) not null,
-   Auth_ID     char(9) not null,
-   
+  (DiagID     char(9) not null,
+   AuthID     char(9) not null);
 
-#Diagram_Authors
+# Diagram_Authors
 create table Diagram_Authors
   (AuthID      char(9) not null,
    Author      varchar(30),
