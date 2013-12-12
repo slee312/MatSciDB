a030d68488bd5166c70b5fd1068da6f2abe794c8
Kyle McGill <kylemcgill18@gmail.com>
2013-12-12 13:14:09 -0500

    added comments with examples as requested

--- ddl2.sql
+++ ddl2.sql
@@ -1,6 +1,9 @@
 # DDL specs, second half
 
-# phase diagram
+#Phase_Diagram							
+#MaterialID	PhaseDiagram	PublicationYear	DiagramType		ConcentrationRange			Temperature	NatureOfInvestigation	DetailsID
+#mp-609465	aHR0cDovL3d3dy5	1991		binary phase diagram	full decomposition; 0-100 at. % Au	950 to 1070 °C	calculated		mp-609465
+
 create table Phase_Diagram
   (MaterialID            char(9) not null,
    PhaseDiagram          blob not null,
@@ -12,7 +15,10 @@ create table Phase_Diagram
    DetailsID             char(9),
    primary key(MaterialID));
 
-# diagram details
+#Diagram_Details									
+#MaterialID	APDICDiagram	UniqueID	Title		Publication	Language	OriginalDiagram	OriginalScope				OriginalSize	Remarks
+#mp-609465	No		904518		Au-Cu alloy	Kexue Tongbao	Engl. Transl.	English		T[947-1067 °C] vs. Au conc.[0-100 at.%]	36/59	
+
 create table Diagram_Details
   (MaterialID      char(9) not null,
    APDICDiagram    tinyint(1),
@@ -26,12 +32,18 @@ create table Diagram_Details
    Remarks         varchar(50),
    primary key(MaterialID));
 
-# Has_Auth
+#Has_Auth	
+#DiagID		AuthID
+#mp-609465	609203
+
 create table Has_Auth
   (DiagID     char(9) not null,
    AuthID     char(9) not null);
 
-# Diagram_Authors
+#Diagram_Authors		
+#AuthID	Author		Affiliation
+#609203	Zheng W.T.	Jilin University, Department of Materials Science
+
 create table Diagram_Authors
   (AuthID      char(9) not null,
    Author      varchar(30),
